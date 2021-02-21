f46f2f15388e9f3ce8df30a3b1c9026c696822f7
Allegro Dino <dinodiegus@gmail.com>
2021-02-20 19:37:40 -0500

    push changes to heroku

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+    MongoDB: 'mongodb+srv://onlinedatingappb:2021@onlinedatingappb.y9cxe.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    FacebookAppID: '416761639413736',
+    FacebookAppSecret: '8464f9218525b2bfcc02a354827b9fcc',
+    GoogleClientID: '815803612681-h8mv7rvoo7vjd7crockbcpf6fadk0pvu.apps.googleusercontent.com',
+    GoogleClientSecret: 'JXGaK1mQaXnq_ILHKzDOY7jl'
+    
+};
