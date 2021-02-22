37edef28aabacbc009f2e95e48be90e231efc426
Allegro Dino <dinodiegus@gmail.com>
2021-02-22 16:06:55 -0500

    google client id

--- config/prod.js
+++ config/prod.js
@@ -2,7 +2,7 @@ module.exports = {
     MongoDB: process.env.MONGO_DB,
     FacebookAppID: process.env.FB_APP_ID,
     FacebookAppSecret: process.env.FB_APP_SECRET,
-    GoogleClientID: '815803612681-h8mv7rvoo7vjd7crockbcpf6fadk0pvu.apps.googleusercontent.com',
-    GoogleClientSecret: 'JXGaK1mQaXnq_ILHKzDOY7jl'
+    GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
     
 };
