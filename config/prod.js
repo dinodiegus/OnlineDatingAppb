10ff804cac88855276b95d7bf8e3716196f2dad2
Allegro Dino <dinodiegus@gmail.com>
2021-02-22 15:25:11 -0500

    facebook app updated

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
     MongoDB: process.env.MONGO_DB,
-    FacebookAppID: '416761639413736',
-    FacebookAppSecret: '8464f9218525b2bfcc02a354827b9fcc',
+    FacebookAppID: process.env.FB_APP_ID,
+    FacebookAppSecret: process.env.FB_APP_SECRET,
     GoogleClientID: '815803612681-h8mv7rvoo7vjd7crockbcpf6fadk0pvu.apps.googleusercontent.com',
     GoogleClientSecret: 'JXGaK1mQaXnq_ILHKzDOY7jl'
     
