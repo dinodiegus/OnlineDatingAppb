4c0fc836908a5110beb9e8529aaf77a5aa01a2f6
Allegro Dino <dinodiegus@gmail.com>
2021-02-22 13:56:00 -0500

    mongoDB link updated

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MongoDB: 'mongodb+srv://onlinedatingappb:2021@onlinedatingappb.y9cxe.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    MongoDB: process.env.MONGO_DB,
     FacebookAppID: '416761639413736',
     FacebookAppSecret: '8464f9218525b2bfcc02a354827b9fcc',
     GoogleClientID: '815803612681-h8mv7rvoo7vjd7crockbcpf6fadk0pvu.apps.googleusercontent.com',
