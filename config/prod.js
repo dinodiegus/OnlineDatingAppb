ca425fa4264102721c9638f1543dc2aa0746b531
Allegro Dino <dinodiegus@gmail.com>
2021-02-24 13:04:54 -0500

    aws id and secret added

--- config/prod.js
+++ config/prod.js
@@ -3,6 +3,8 @@ module.exports = {
     FacebookAppID: process.env.FB_APP_ID,
     FacebookAppSecret: process.env.FB_APP_SECRET,
     GoogleClientID: process.env.GOOGLE_CLIENT_ID,
-    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
+    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    AWSAccessKeyID: process.env.AWS_ACCESS_KEY,
+    AWSAccessKeySecret: process.env.AWS_ACCESS_SECRET
     
 };
